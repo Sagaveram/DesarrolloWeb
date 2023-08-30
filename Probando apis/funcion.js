bf10cd2bdcbdee30a89d0c8149cf24a5c7ae3fd7
Sagaveram <110860320+Sagaveram@users.noreply.github.com>
2023-08-30 20:00:11 -0300

    Se muestran personajes dependiendo del indice ingresado

--- Probando apis/funcion.js
+++ Probando apis/funcion.js
@@ -1,43 +1,55 @@
-let personajes = [];
 
-function getCharacters(done){
-    const results=fetch("https://rickandmortyapi.com/api/character")
+const url = "https://rickandmortyapi.com/api/character";
 
-    results.then(response => response.json())
-    .then(data =>{
-        done(data)
-    });
-
-}
-
-const url="https://rickandmortyapi.com/api/character";
-/*getCharacters(data => {
-    console.log(data);
-    
-});*/
-    
-async function fetchData() {
+async function fetchData(characterIndex) {
   try {
-const response = await fetch(url, { method: "GET" });
-    const data = await response.json(); // extract JSON from response
-    personajes=data;
-    let tarjeta1 = document.getElementById("nombre1");
-    
-    let nuevaTarjeta = document.createElement("div");
-    nuevaTarjeta.className="card";
-    nuevaTarjeta.style="width:75%";
-    console.log(personajes);
-    nuevaTarjeta.innerHTML = personajes[0].name;
-    
-    tarjeta1.appendChild(nuevaTarjeta);
-} catch (error) {
-console.log("Error fetching data: ", error);
+    const response = await fetch(url, { method: "GET" });
+    const data = await response.json();
+    const personajes = data.results; // Asumiendo que los personajes están en data.results
+
+    if (personajes.length > characterIndex) {
+      const tarjeta1 = document.getElementById("nombre1");
+      tarjeta1.innerHTML = ""; // Limpia el contenido previo si lo hubiera
+
+      const nuevaTarjeta = document.createElement("div");
+      nuevaTarjeta.className = "card";
+      nuevaTarjeta.style = "width:75%";
+
+      const imagen = document.createElement("img");
+      imagen.src = personajes[characterIndex].image;
+
+      const nombrePersonaje = document.createElement("p");
+      nombrePersonaje.textContent = personajes[characterIndex].name;
+
+      nuevaTarjeta.appendChild(imagen);
+      nuevaTarjeta.appendChild(nombrePersonaje);
+
+      tarjeta1.appendChild(nuevaTarjeta);
+    } else {
+      console.log("Índice de personaje inválido.");
+      alert("Fuera de indice")
+    }
+  } catch (error) {
+    console.log("Error fetching data: ", error);
+  }
 }
- }
 
-const boton1 = document.getElementById("boton1");
+const botonMostrar = document.getElementById("botonMostrar");
+botonMostrar.addEventListener("click", function () {
+  const indiceInput = document.getElementById("indicePersonaje");
+  const characterIndex = parseInt(indiceInput.value, 10); // Convierte el valor a entero
+  fetchData(characterIndex);
+});
 
-boton1.addEventListener("click",fetchData);
+const indiceInput = document.getElementById("indicePersonaje");
+indiceInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      const characterIndex = parseInt(indiceInput.value, 10);
+      fetchData(characterIndex);
+    }
+  });
+
+
 
   
 
