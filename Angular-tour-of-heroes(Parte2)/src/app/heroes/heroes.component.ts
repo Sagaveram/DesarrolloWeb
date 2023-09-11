1495df50426f5dd86703640485cc296d31b5dd6a
Sagaveram <110860320+Sagaveram@users.noreply.github.com>
2023-09-11 20:50:12 -0300

    Hasta la parte de OBSERVABLES

--- Angular-tour-of-heroes(Parte2)/src/app/heroes/heroes.component.ts
+++ Angular-tour-of-heroes(Parte2)/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -10,7 +10,20 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent {
    
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  
+
+
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
